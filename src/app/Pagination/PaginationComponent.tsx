92d08dd6e98e689adaf5b4ef4585526fc6c6dae8
joe-jngigi <josephngigi775@gmail.com>
2023-08-28 23:30:35 +0300

    Next: Recreate app

---
+++ src/app/Pagination/PaginationComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PaginationComp = () => {
+  return (
+    <div className='dark:bg-slate-950 p-3 rounded-2xl mt-7 min-h-590'>
+      <div>Pagination</div>
+    </div>
+  )
+}
+
+export default PaginationComp
---
+++ src/app/context_page/layout.tsx
@@ -0,0 +1,55 @@
+'use client'
+import React, { useContext, useState } from 'react'
+import Link from 'next/link'
+import {MdOutlineLightMode, MdOutlineDarkMode} from 'react-icons/md'
+
+import { ThemeContext, defaultThemeValue } from './context/ThemeContext'
+import { ThemeTypes } from '../typescript/types/types'
+
+const ProductsLayout = ({children}: {children: React.ReactNode}) => {
+
+  // by default it will always be light. So that means I will pass this into the context provider
+  const [themeValue, setThemeValue] = useState<'light' | 'dark' >(defaultThemeValue.themeValue) 
+  const [ThemeChange, setthemeChange] = useState(false)
+  
+
+  const changeTheme = (e: React.MouseEvent<HTMLButtonElement>) =>{
+    e.preventDefault()
+    setthemeChange(!ThemeChange)
+    console.log(ThemeChange);
+    setThemeValue((prev) => prev === 'dark' ? 'light' : 'dark')
+    
+  }
+
+  return (
+    <div className='min-h-590'>
+      <ThemeContext.Provider value={{themeValue}}>
+        <h1 className='text-4xl font-extrabold mt-5 dark:text-cyan-200 text-slate-900'>Dev Page for Context API</h1>
+        
+        <div className='flex justify-between items-center'>
+          <div className='mt-5 mb-3 duration-300 transition-all hover:translate-x-1 flex gap-5'>
+            <Link className=' py-2 px-6 rounded-full border-2 duration-700 transition-all hover:text-black hover:dark:bg-green-200' href = '/context_page'>
+              Products page
+            </Link>
+
+            <Link className=' py-2 px-6 rounded-full border-2 duration-700 transition-all hover:text-black hover:dark:bg-green-200' href = '/context_page/addProduct'>
+              Add Product
+            </Link>
+          </div>
+
+          <div>
+            <button className='text-2xl' onClick={changeTheme}>
+              {ThemeChange ? ( <MdOutlineDarkMode/> ):(<MdOutlineLightMode />)}
+            </button>
+          </div>
+        </div>
+
+        <div className='dark:bg-slate-950 p-3 rounded-2xl mt-10 '>
+          {children}
+        </div>
+      </ThemeContext.Provider>
+    </div>
+  )
+}
+
+export default ProductsLayout
